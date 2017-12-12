8bd7a70bb5ab2bd294693b0ccc4817ebf293cf24
Andrew Lineyschikov <alineyschikov@gmcs.ru>
2017-12-12 17:45:04 +0300

    Completed the tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HeroService } from './hero.service';
+import { InMemoryDataService }  from './in-memory-data.service';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessageComponent } from './message/message.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroSearchComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
